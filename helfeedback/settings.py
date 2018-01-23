65057dc0846125ca7d1554b86c62c54268ba55cc
Ville Koivunen <ville.koivunen@hel.fi>
2018-01-23 18:10:34 +0200

    Minimally working state of the SCSS rendering

--- helfeedback/settings.py
+++ helfeedback/settings.py
@@ -101,6 +101,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'djcelery',
+    'compressor',
 
     # Apps within this repository
     'feedback'
@@ -109,6 +110,20 @@ INSTALLED_APPS = [
 # Rest of configuration is above together with others
 if env('SENTRY_DSN'):
     INSTALLED_APPS.append('raven.contrib.django.raven_compat')
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "node_modules/"),
+]
+
+COMPRESS_PRECOMPILERS = [
+    ('text/x-scss', 'django_libsass.SassCompiler'),
+]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
