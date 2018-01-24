d7acb5739ca2d647a73412dc0891b090054906e0
Ville Koivunen <ville.koivunen@hel.fi>
2018-01-24 23:39:13 +0200

    urls: remove unneeded imports

--- helfeedback/urls.py
+++ helfeedback/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib import admin
 from django.views.decorators.clickjacking import xframe_options_exempt
 from django.views.generic.base import RedirectView
@@ -21,7 +21,6 @@ from django.views.generic.base import RedirectView
 # from feedback.api import APIRouter
 from feedback.views import FeedbackView, FeedbackCreate
 from django.views.generic import TemplateView
-from django.conf import settings
 
 admin.autodiscover()
 
